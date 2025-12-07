7cc8ec11ccfb1fa064eb63f86335bc761030e6c1
AndresCRamos <57693598+AndresCRamos@users.noreply.github.com>
2025-12-06 19:03:16 -0500

    Add views to model ShortenURLModel

--- internal/shortener/model.go
+++ internal/shortener/model.go
@@ -3,8 +3,9 @@ package shortener
 import "time"
 
 type ShortenURLModel struct {
-	ID            int
-	Original      string
-	Short         string
-	CreatedAt     time.Time
+	ID        int
+	Original  string
+	Short     string
+	CreatedAt time.Time
+	Views     int
 }
